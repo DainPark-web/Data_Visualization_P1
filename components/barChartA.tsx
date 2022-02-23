52abce4ce1c18e502a2e389aeede84de2c7dfe68
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-23 15:59:16 +0100

    update

--- components/barChartA.tsx
+++ components/barChartA.tsx
@@ -1,8 +1,45 @@
 // BarChart 01
+import * as d3 from "d3"
+import { useEffect, useRef } from "react";
 
 const BarChartA =() => {
+    const svgContainer:any = useRef<HTMLDivElement>();
+
+   
+    
+    useEffect(() => {
+        const WIDTH = svgContainer.current.clientWidth;
+        const HEIGHT = svgContainer.current.clientHeight;
+        
+        const margin = {
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: 20,
+        }
+        
+        const svg = d3
+        .select("svg")
+        .style("background", "orange");
+
+
+
+    },[])
     return (
-        <div>ChartA</div>
+        <>
+        <div ref={svgContainer} className="svgContainer">
+            <svg>
+
+            </svg>
+        </div>
+        <style jsx>{`
+            .svgContainer{
+                width: 100vw;
+                height: 100vh;
+                background: orange;
+            }
+        `}</style>
+        </>
     )
 }
 
