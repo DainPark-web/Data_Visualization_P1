6dea7919085bf244a0106527faf13f36878799f7
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-25 12:46:03 +0100

    update

--- components/ListBox.tsx
+++ components/ListBox.tsx
@@ -1,6 +1,26 @@
-const ListBox = () => {
+import Link from "next/link";
+
+
+interface IListBox {
+    pathName: string;
+    listName: string;
+}
+const ListBox = ({pathName, listName}: IListBox) => {
     return (
         <>
+           <Link href={`/${pathName}`}>
+               <a className="aLinks">
+                   <div>
+                       {listName}
+                   </div>
+                </a>
+           </Link> 
+           <style jsx>{`
+            .aLinks{
+                
+            }
+               
+        `}</style>
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // import * as d3 from "d3";
 import Link from "next/link";
 import Head from "next/head";
+import ListBox from "../components/ListBox";
 
 const Home = () => {
   return (
@@ -10,12 +11,7 @@ const Home = () => {
     </Head>
     <main>
       <div className="listContainer">
-        <Link href="/barChart">
-          <a className="aLinks" >Bar Chart</a>
-        </Link>
-        <Link href="/about">
-          <a className="aLinks" >Pie Chart</a>
-        </Link>
+        <ListBox pathName="/barChart" listName="BarChart" />
       </div>
      
     </main>
@@ -28,44 +24,24 @@ const Home = () => {
         background: white;
 
         display: flex;
-        justify-content: flex-start;
+        justify-content: center;
         align-items: flex-start;
-        position: relative;
-
-        flex-direction: column;
-
-      }
-
-     
       
 
+      }
       .listContainer{
-          position: absolute;
-          bottom: 50px;
-          left: 50px;
           width: 70vw;
+          min-height: 100vh;
+          padding-top: 80px;
       
           display: grid;
           grid-template-columns: repeat(4, 1fr);
           grid-gap: 20px;
       }
+     
+      
 
-      .aLinks{
-          padding: 10px 20px;
-          background: none;
-          border: 2px solid black;
-          border-radius: 5px;
-          text-align: center;
-          text-decoration: none;
-          color: black;
-          font-size: 20px;
-          font-weight: 500;
-          transition: 0.3s ease-in-out;   
-          &:hover{
-          color: white;
-          background-color: black;
-      }
-      }
+      
 
       
 
