b0918386bcd0c77092f6bb7142131c0e63bfbb08
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-26 06:26:42 +0100

    update

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,10 +12,18 @@ const About = () => {
                     Über das Projekt
                 </h2>
                 <p>
-                    Das Projekt ist für Datei Visualisierung.
+                    Das ist meine kleine Projekt. Es geht um Datei Visualisierung.
                     Besonders wollte ich verschiedene Charts probieren. 
                     Dafür wollte ich auch Datei vorbereiten und analysieren.
                     Wenn Sie sich für das Code interessieren, dann können Sie in meinem <Link href="https://github.com/DainPark-web/Data_Visualization_P1"><a className="githubT">Github</a></Link> sehen. 
+                </p>
+                <h2>
+                    Über mich
+                </h2>
+                <p>
+                    Hi! Ich bin Dain Park. Jetzt wohne ich in Berlin und studiere Information Design in Universität der Kunst in Berlin.
+                    Und dafür arbeite ich mit coding zusammen. Ich liebe programming und werde als Frondend Developer in der Zukunft arbeiten.
+                     <Link href="https://github.com/DainPark-web"><a className="githubT">Github</a></Link>
                 </p>
             </div>
         </div>
@@ -25,7 +33,7 @@ const About = () => {
                 height: 100vh;
                 background: white;
                 padding: 0 20px;
-                padding-top: 100px;
+                padding-top: 150px;
                 display: flex;
                 justify-content: center;
             }
@@ -36,6 +44,7 @@ const About = () => {
             }
             p{
                 font-size: 20px;
+                margin-bottom: 200px;
             }
             .githubT{
                 color: royalblue;
