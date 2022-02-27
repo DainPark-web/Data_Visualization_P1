49f0f13e29399d04994070773575eb80dd4a7b2c
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-27 18:40:05 +0100

    update

--- components/pieChartA.tsx
+++ components/pieChartA.tsx
@@ -1,8 +1,64 @@
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+
 const PieChartA = () => {
-    
+    const containerRef:any = useRef<HTMLDivElement>(null);
+
+
+
+    useEffect(() => {
+        const data = [10, 40, 30, 142, 35, 96, 43, 21, 67, 53];
+        const pieGenerator = d3.pie();
+        const arcData = pieGenerator(data);
+
+        const WIDTH = containerRef.current.clientWidth;
+        const HEIGHT = containerRef.current.clientHeight;
+
+
+        const rSize = HEIGHT/2;
+        const rW = HEIGHT/2.5;
+        const arcGenerator:any = d3
+        .arc()
+        .innerRadius(rSize-rW)
+        .outerRadius(rSize)
+        .padAngle(.02)
+        .padRadius(100)
+        .cornerRadius(4);
+
+
+
+        const svg = d3
+        .select(".svgContainer")
+        .append("svg")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT)
+        
+
+        const mainG = svg
+        .append("g")
+        .attr("transform", `translate(${WIDTH/2},${HEIGHT/2})`);
+
+
+        mainG
+        .selectAll("path")
+        .data(arcData)
+        .join("path")
+        .attr("d", arcGenerator)
+        .attr("fill", "royalblue")
+        
+
+
+        
+
+
+
+    },[])
+
+
     return (
         <>
-        <div className="svgContainer">
+        <div ref={containerRef} className="svgContainer">
         </div>
         <style jsx>{`
             .svgContainer{
@@ -11,7 +67,7 @@ const PieChartA = () => {
                 display: flex;
                 justify-content: center;
                 align-items: flex-start;
-                background: red;
+         
                
             }
         `}</style>
